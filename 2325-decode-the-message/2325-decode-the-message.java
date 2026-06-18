0c8da3050c234d515de2c332ae6ae7450c5b6df9
Thiruvikraman123 <146513380+Thiruvikraman123@users.noreply.github.com>
2026-06-18 19:08:01 +0530

    Time: 7 ms (65.32%), Space: 44.7 MB (71.26%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        HashMap<Character,Character> map=new HashMap<>();
+
+        char ch='a';
+
+        for(int i=0;i<key.length();i++)
+        {
+            char c=key.charAt(i);
+            if(c!=' ' && !map.containsKey(c))
+            {
+                map.put(c,ch);
+                ch++;
+            }
+        }
+        StringBuilder ans=new StringBuilder();
+
+        for(int i=0;i<message.length();i++)
+        {
+            char c=message.charAt(i);
+            if(c==' ')
+            {
+                ans.append(' ');
+            }
+            else{
+                ans.append(map.get(c));
+            }
+
+        }
+        return ans.toString();
+
+        
+    }
+}
